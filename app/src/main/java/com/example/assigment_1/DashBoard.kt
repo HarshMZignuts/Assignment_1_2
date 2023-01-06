543024cb40735be42cd120ba846ef78ab0b35f61
harsh317 <mistryhk317@gmail.com>
2023-01-06 13:23:02 +0530

    Bottom Navigation added

--- app/src/main/java/com/example/assigment_1/DashBoard.kt
+++ app/src/main/java/com/example/assigment_1/DashBoard.kt
@@ -1,9 +1,11 @@
 package com.example.assigment_1
 
 import android.content.SharedPreferences
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_dash_board.*
 import kotlinx.android.synthetic.main.activity_login_screen.*
 
@@ -14,13 +16,44 @@ class DashBoard : AppCompatActivity() {
         setContentView(R.layout.activity_dash_board)
         var helper = Helper(applicationContext)
         db = helper.readableDatabase
+
         val SharedPreferences : SharedPreferences = getSharedPreferences("MyPreff", MODE_PRIVATE)
 
         var mymail = SharedPreferences.getString("mail","").toString()
-    var a = dash_tv.text.toString()
-        val rs = db.execSQL("SELECT Name FROM Table_Client WHERE Email == $mymail")
+            //dash_tv.text = mymail
+             //var a = dash_tv.text.toString()
+            //val rs = db.execSQL("SELECT Name FROM Table_Client WHERE Email == $mymail")
+            // val rs =  db.rawQuery("SELECT Name FROM Table_Client WHERE Email == $mymail",null)
+           //dash_tv.text = helper.getname(mymail).toString()
+        changeFragment(HomeFragment())
+        bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId)
+            {
+                R.id.menu_home -> changeFragment(HomeFragment())
+                R.id.menu_vendors -> changeFragment(VendorFragment())
+                R.id.menu_category -> changeFragment(CategoryFragment())
+                R.id.menu_list -> changeFragment(ListFragment())
+                R.id.menu_more -> changeFragment(MoreFragment())
+                else->{
 
-            dash_tv.text = rs.toString()
+
+                }
+            }
+            true
+
+        }
+
+
+
+    }
+
+    private fun changeFragment(fragment : Fragment)
+    {
+        val fragmentmanger =  supportFragmentManager
+        val fragmenttransaction =  fragmentmanger.beginTransaction()
+        fragmenttransaction.replace(R.id.dash_frame,fragment)
+        fragmenttransaction.commit()
+
 
     }
 }
--- app/src/main/java/com/example/assigment_1/Helper.kt
+++ app/src/main/java/com/example/assigment_1/Helper.kt
@@ -2,6 +2,7 @@ package com.example.assigment_1
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
@@ -20,14 +21,17 @@ class Helper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DATAB
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE $TBL_CLIENT ($ID integer primary key autoincrement,$NAME text,$EMAIL text,$PASSWORD text)")
+        db?.execSQL("CREATE TABLE $TBL_CLIENT ($ID integer primary key autoincrement,$NAME text,$EMAIL email,$PASSWORD text)")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
 
     }
 
-
-
+//fun getname (email:String)
+//{
+//    var db = this.readableDatabase
+//    return db.execSQL("SELECT Name FROM Table_Client WHERE Email == $email")
+//}
 
 }
